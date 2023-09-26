217f64901edfb83536683cada430f9195ac21d66
Ананьев Илья Дмитриевич <23618@dune.edu>
2023-09-26 10:59:55 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <windows.h>
+#include<math.h>
+
+using namespace std;
+int main()
+{
+    int x, y, z, a, b , c ;
+
+    // Ввод исходных данных
+    cout << "Input x" << endl;
+    cin >> x;
+    cout << "Input y" << endl;
+    cin>>y;
+    cout << "Input z" << endl;
+    cin>>z;
+        }
+    }
+
+    // Обработка данных
+    a=(2*cos(x-3.14/6);
+    b=(1+(pow(z,2) z/3.14)-(pow z/5));
+    c=(a*b)/e+ pow sin(y);
+            }
+        }
+    }
+
+    // Вывод результата
+    cout<<endl<<"<<endl;
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            c.X = 5 + (j * 5);
+            c.Y = 7 + i;
+            SetConsoleCursorPosition(hnd, c);
+            cout << B[i][j];
+        }
+    }
+
+    return 0;
+}
